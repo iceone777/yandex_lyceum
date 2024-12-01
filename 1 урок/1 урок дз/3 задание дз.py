fee7e2a97c303d9337a5d867b4e779961c7c5b56
Serega Gorbunov <gorbunov.sergei0808@gmail.com>
2024-12-01 19:57:07 +0300

    home work 1 урок

---
+++ 1 урок/1 урок дз/3 задание дз.py
@@ -0,0 +1,7 @@
+name = input()
+surname = input()
+favorite_animal = input()
+zodiac_sign = input()
+print('Индивидуальный гороскоп для пользователя', name, surname)
+print('Кем вы были в прошлой жизни:', favorite_animal)
+print('Ваш знак зодиака -', zodiac_sign, ', поэтому вы - тонко чувствующая натура.')
